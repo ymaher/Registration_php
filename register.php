5e076fb54f93ad783b78c01db19a77bb53167a5f
yogesh <yogeshmaher64@gmail.com>
2017-12-09 12:20:12 +0530

    Form added

--- register.php
+++ register.php
@@ -11,7 +11,30 @@
 	</div>
 	<form method="post" action="register.php">
 		
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username">
+		</div>
 
+		<div class="input-group">
+			<label>Email</label>
+			<input type="text" name="email">
+		</div>
+		
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		
+		<div class="input-group">
+			<label>Confirm Password</label>
+			<input type="password" name="password_2">
+		</div>
+		
+		<div class="input-group">
+
+			<button type="submit" name="register" class="btn">Register</button>
+		</div>
 
 	</form>
 </body>
