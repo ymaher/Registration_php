2de4a4a2c5da82b8df65d5e46554297413f9c83d
yogesh <yogeshmaher64@gmail.com>
2017-12-09 11:47:07 +0530

    initial commit

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registartion</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Register</h2>
+	</div>
+	<form method="post" action="register.php">
+		
+
+
+	</form>
+</body>
+</html>
